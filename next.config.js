0c79ec2d474d8fdc9026760416dc4c5a8ebbd0e8
wpcodevo <ziddah.edem@gmail.com>
2023-06-02 18:11:55 +0000

    updated

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  rewrites: async () => {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/:path*" : "/api/:path*",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Accept, Content-Type, Authorization" },
-        ],
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
       },
     ];
   },
