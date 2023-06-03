6fde52ba127a1d02cd77fb42fb8b94cd7a8278fd
wpcodevo <ziddah.edem@gmail.com>
2023-06-03 03:44:56 +0000

    updated

--- api/index.py
+++ api/index.py
@@ -1,9 +1,22 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "https://nextjs-fastapi-framework-w5m5e98f4-ziddahedem.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TodoCreate(BaseModel):
